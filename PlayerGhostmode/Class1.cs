cbf51ce69968f19fa487dc51ac0f7f38660393bc
Jack Fisherman <Infernogaming621@gmail.com>
2019-05-23 18:06:32 -0400

    Add project files.

---
+++ PlayerGhostmode/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerGhostmode
+{
+    public class Class1
+    {
+    }
+}
